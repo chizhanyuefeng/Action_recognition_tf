b9650bf9c82025b1c893b7a4bce6b0d8298a758e
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-08-01 20:15:57 +0800

    添加45类

--- c3d_network.py
+++ c3d_network.py
@@ -1,6 +1,17 @@
 import tensorflow as tf
 
 class C3D_Network(object):
+
+    class_label = {"Archery": 0, "BabyCrawling": 1, "BasketballShooting": 2, "BenchPress": 3, "Biking": 4,
+                   "BlowingCandles": 5, "Bowling": 6, "BreatStroke": 7, "CleanAndJerk": 8, "CliffDiving": 9,
+                   "Drumming": 10, "FrisbeeCatch": 11, "GolfSwing": 12, "Haircut": 13, "HeadMassage": 14,
+                   "HighJump": 15, "HorseRiding": 16, "Hug": 17, "HulaHoop": 18, "JumpRope": 19,
+                   "Kayaking": 20, "Kiss": 21, "Laugh": 22, "LongJump": 23, "Marching": 24,
+                   "PlayingCello": 25, "PlayingGuitar": 26, "PlayingPiano": 27, "PlayingViolin": 28,"PoleVault": 29,
+                   "PullUps": 30, "PushUps": 31, "ShakingHands": 32, "SitUp": 33, "Skiing": 34,
+                   "Skijet": 35, "SkyDiving": 36, "SoccerJuggling": 37, "SoccerShooting": 38, "Somersault": 39,
+                   "TaiChi": 40, "ThrowDiscus": 41, "TrampolineJumping": 42, "WalkingWithDog": 43, "WashingHair": 44,}
+
     num_classes = 45
     channels = 3
 
@@ -52,26 +63,6 @@ class C3D_Network(object):
         # Output: class prediction
         out = tf.matmul(dense2, self._weights['out']) + self._biases['out']
 
-
-    def _conv3d(self, name, input, filter, bias):
-        res_conv = tf.nn.conv3d(input, filter, [1, 1, 1, 1], padding="SAME", name=name)
-        res = tf.nn.bias_add(res_conv, bias)
-        return res
-    def _max_pool(self, name, input, k):
-        return tf.nn.max_pool3d(input, ksize=[1, k, 2, 2, 1], strides=[1, k, 2, 2, 1], padding="SAME", name=name)
-
-    def _variable_on_cpu(self, name, shape, initializer):
-        with tf.device('/cpu:0'):
-            var = tf.get_variable(name, shape, initializer=initializer)
-        return var
-
-    def _variable_with_weight_decay(self, name, shape, stddev, punish_lambda=None):
-        var = self._variable_on_cpu(name, shape, tf.truncated_normal_initializer(stddev=stddev))
-        if punish_lambda is not None:
-            weight_decay = tf.nn.l2_loss(var) * punish_lambda
-            tf.add_to_collection('losses', weight_decay)
-        return var
-
     def _parameters_config(self):
         with tf.variable_scope('var_name'):
             self._weights = {
@@ -101,6 +92,24 @@ class C3D_Network(object):
                 'out': self._variable_with_weight_decay('bout', [self.num_classes], 0.04, 0.0),
             }
 
+    def _conv3d(self, name, input, filter, bias):
+        res_conv = tf.nn.conv3d(input, filter, [1, 1, 1, 1], padding="SAME", name=name)
+        res = tf.nn.bias_add(res_conv, bias)
+        return res
+    def _max_pool(self, name, input, k):
+        return tf.nn.max_pool3d(input, ksize=[1, k, 2, 2, 1], strides=[1, k, 2, 2, 1], padding="SAME", name=name)
+
+    def _variable_on_cpu(self, name, shape, initializer):
+        with tf.device('/cpu:0'):
+            var = tf.get_variable(name, shape, initializer=initializer)
+        return var
+
+    def _variable_with_weight_decay(self, name, shape, stddev, punish_lambda=None):
+        var = self._variable_on_cpu(name, shape, tf.truncated_normal_initializer(stddev=stddev))
+        if punish_lambda is not None:
+            weight_decay = tf.nn.l2_loss(var) * punish_lambda
+            tf.add_to_collection('losses', weight_decay)
+        return var
 
 
 
