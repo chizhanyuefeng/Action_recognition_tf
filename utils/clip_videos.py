94cf406a56e0fa60333e903679925c642dc7ea29
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-08-01 16:23:53 +0800

    进一步完成数据集提取工作

--- utils/clip_videos.py
+++ utils/clip_videos.py
@@ -1,12 +1,14 @@
-import numpy as np
+import os
 import cv2
 
-VIDEO_PATH = '/home/tony/motion_research/FutureCamp_ActionRecognitionData_TrainVal'
+DATASET_PATH = '/home/tony/motion_research/FutureCamp_ActionRecognitionData_TrainVal'
+SAVE_PATH = '../dataset/'
+FRAMES_SPACE = 3
 
-def save_frames_from_video(video_path,frames_space):
-
+def save_frames_from_video(video_path, save_path, frames_space):
     capture = cv2.VideoCapture(video_path)
     n = 0
+    i = 0
     while (True):
         n=n+1
         ret, frame = capture.read()
@@ -14,7 +16,24 @@ def save_frames_from_video(video_path,frames_space):
         if frame is None:
             break
         if n%frames_space == 0:
-            cv2.imwrite(str(n)+'.jpg', img)
+            i = i + 1
+            cv2.imwrite(save_path + str(i)+'.jpg', img)
     capture.release()
 
-save_frames_from_video(VIDEO_PATH+'/GolfSwing/v_GolfSwing_g01_c01.avi', 3)
+def extract_all_videos(path):
+    if not os.path.exists(path):
+        print('path is not exist：', path)
+        return None
+
+    for i in os.listdir(path):
+        cur_path = path + "/" + i
+        if os.path.isfile(cur_path) and ('avi' in i):
+            video_name = cur_path.replace(DATASET_PATH, '').replace('.avi', '')
+            save_path = SAVE_PATH + video_name + '/'
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            save_frames_from_video(cur_path, save_path, FRAMES_SPACE)
+        else:
+            extract_all_videos(cur_path)
+
+extract_all_videos(DATASET_PATH)
