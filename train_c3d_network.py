f31dc92989b99115de15dd20bcd749edd8902e88
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-08-02 23:55:42 +0800

    添加tensorboard输出

--- train_c3d_network.py
+++ train_c3d_network.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import tensorflow as tf
 from dataset import Dataset
@@ -14,6 +15,7 @@ class Train_C3D_Network(object):
         self.batch_size = batch_size
         self.train_step = train_step
         self.depth = depth
+        self._train_logger_init()
 
     def train(self):
         x = tf.placeholder(tf.float32, shape=[self.batch_size,
@@ -23,7 +25,7 @@ class Train_C3D_Network(object):
                                               3])
 
         label = tf.placeholder(tf.float32, shape=[self.batch_size, len(class_label.keys())])
-        network = C3D_Network(x, self.batch_size, dropout_prob=0.5, trainable=True)
+        network = C3D_Network(x, self.batch_size, dropout_prob=1, trainable=True)
         net_predict = network.contruct_graph()
 
         # 计算loss
@@ -33,6 +35,7 @@ class Train_C3D_Network(object):
 
         with tf.name_scope('total_loss'):
             total_loss = tf.add_n(tf.get_collection('losses'))
+            tf.summary.scalar("total_loss", total_loss)
 
         with tf.name_scope('optimizer'):
             train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(total_loss)
@@ -41,26 +44,58 @@ class Train_C3D_Network(object):
             correct_prediction = tf.equal(tf.argmax(net_predict, 1), tf.argmax(label, 1))
             correct_prediction = tf.cast(correct_prediction, tf.float32)
             accuracy = tf.reduce_mean(correct_prediction)
+            tf.summary.scalar("accuracy", accuracy)
 
+        # 保存模型
         saver = tf.train.Saver()
+        # tensorboard
+        merged = tf.summary.merge_all()
 
         data = Dataset(self.batch_size, self.depth, self.img_size)
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+            train_writer = tf.summary.FileWriter("./tensorboard_logs/", sess.graph)
 
             for step in range(1, self.train_step+1):
                 train_x, train_y = data.get_next_batch()
-
                 sess.run(train_op, feed_dict={x: train_x, label: train_y})
-                if step%1 ==0:
+                summ = sess.run(merged, feed_dict={x: train_x, label: train_y})
+                train_writer.add_summary(summ, global_step=step)
+
+                if step%5 ==0:
                     res = sess.run([total_loss, accuracy], feed_dict={x: train_x, label: train_y})
-                    print('step:%d, accuracy: %6f, total loss: %6f' % (step, res[1], res[0]))
+                    self.train_logger.info('step:%d, accuracy: %6f, total loss: %6f' % (step, res[1], res[0]))
                 if step%100 == 0:
                     save_path = saver.save(sess, self.model_save_path)
-                    print('model saved at', save_path)
+                    self.train_logger.info('model saved at', save_path)
+
+            train_writer.close()
+
+    def _train_logger_init(self):
+        """
+        初始化log日志
+        :return:
+        """
+        self.train_logger = logging.getLogger('train')
+        self.train_logger.setLevel(logging.DEBUG)
+
+        # 添加文件输出
+        log_file = './train_logs/' + time.strftime('%Y%m%d%H%M', time.localtime(time.time())) + '.logs'
+        file_handler = logging.FileHandler(log_file, mode='w')
+        file_handler.setLevel(logging.DEBUG)
+        file_formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+        file_handler.setFormatter(file_formatter)
+        self.train_logger.addHandler(file_handler)
+
+        # 添加控制台输出
+        consol_handler = logging.StreamHandler()
+        consol_handler.setLevel(logging.DEBUG)
+        consol_formatter = logging.Formatter('%(message)s')
+        consol_handler.setFormatter(consol_formatter)
+        self.train_logger.addHandler(consol_handler)
 
 
 if __name__=="__main__":
-    train = Train_C3D_Network(batch_size=10)
+    train = Train_C3D_Network(batch_size=20)
     train.train()
